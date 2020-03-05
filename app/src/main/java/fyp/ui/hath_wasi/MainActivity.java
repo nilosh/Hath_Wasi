46214ef8d93cf6d408b7e380b7ac5f363a917412
nilosh <nilosh@users.noreply.github.com>
2020-03-05 23:34:37 +0530

    Initial Commit

---
+++ app/src/main/java/fyp/ui/hath_wasi/MainActivity.java
@@ -0,0 +1,14 @@
+package fyp.ui.hath_wasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
