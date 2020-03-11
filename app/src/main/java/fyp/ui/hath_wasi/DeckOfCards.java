2b1e8c20ae51e3bb8b8703b82f9ea7205b488699
nilosh <nilosh@users.noreply.github.com>
2020-03-11 11:33:53 +0530

    Creating the initial classes.  - Card Class  - Deck of Cards

---
+++ app/src/main/java/fyp/ui/hath_wasi/DeckOfCards.java
@@ -0,0 +1,12 @@
+package fyp.ui.hath_wasi;
+
+import java.util.ArrayList;
+
+public class DeckOfCards {
+
+    ArrayList<Card> cardDeck = new ArrayList<>();
+
+    public DeckOfCards() {
+
+    }
+}
