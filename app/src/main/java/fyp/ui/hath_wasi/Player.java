bef3817e342bc6bf1aa8c732f7b1bd5c43410ce8
thejani4950 <thejaniaravinda4950@gmail.com>
2020-03-13 08:06:17 +0530

    Update Player.java
    Removed test code

--- app/src/main/java/fyp/ui/hath_wasi/Player.java
+++ app/src/main/java/fyp/ui/hath_wasi/Player.java
@@ -1,7 +1,5 @@
 package fyp.ui.hath_wasi;
 
-import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -10,15 +8,14 @@ public class Player {
     private String name;
     private int score;
     protected int numberOfCardsRemaining;
-    private boolean trumpCalled;
 
-    protected ArrayList <Card> CardDeck = new ArrayList();
+    ArrayList <Card> CardDeck = new ArrayList();
+
 
     //instance of a Player
     public Player(String name, DeckOfCards cardDeck ) {
         this.name = name;
         this.score = 0;
-        this.trumpCalled = false;
         this.numberOfCardsRemaining = 12;
         this.CardDeck = cardDeck.DealingCards();
         //Sort the card deck in hand
@@ -59,6 +56,7 @@ public class Player {
         return this.CardDeck.size();
     }
 
+
     //take a card type as an input and return true if the given card type exist within the players cards
     public boolean CheckForCardType(String checkType){
         for (int i = 0; i < this.numberOfCardsRemaining; i++){
@@ -69,21 +67,7 @@ public class Player {
         return false;
     }
 
-    public void setTrump(boolean trumpCalled){this.trumpCalled=trumpCalled;}
 
-    // deal a hand of cards to the player and sort it
-    public void setNewCards(DeckOfCards cardDeck){
-        this.CardDeck = cardDeck.DealingCards();
-        Collections.sort(this.CardDeck);
-    }
-
-    public int getNumberOfCardsRemaining() {
-        return numberOfCardsRemaining;
-    }
-
-    public boolean getTrump(){ return trumpCalled;}
-
-    public void setNumberOfCardsRemaining(int numberOfCardsRemaining) {
-        this.numberOfCardsRemaining = numberOfCardsRemaining;
-    }
 }
+
+
